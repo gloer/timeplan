a425dd20936bd5a558410a8db33bfa777dd94660
gloer <gloer@langslet.com>
2021-01-20 23:44:24 +0100

    Added Aksel

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,7 @@
 <script>
 	import { onMount } from "svelte"
 	let navn = "Aksel"
+	let august = true
 
 	let container
 
@@ -60,12 +61,79 @@
 			]
 		}
 	]
+
+	let timeplanAksel = [
+		{
+			dag: "mandag",
+			timer: [
+				{fag: "Veiledning", tid: "09:30 - 10:00"},
+				{fag: "Musikk", tid: "10:15 - 11:15"},
+				{fag: "Norsk", tid: "12:00 - 13:00"},
+				{fag: "KRLE", tid: "13:15 - 14:15"},
+				{fag: "Tysk", tid: "15:00 - 16:00"}
+			]
+		},
+		{
+			dag: "tirsdag",
+			timer: [
+				{fag: "Norsk", tid: "08:30 - 10:00"},
+				{fag: "Naturfag", tid: "10:15 - 11:15"},
+				{fag: "Matte", tid: "12:00 - 13:00"},				
+				{fag: "Samfunnsfag", tid: "13:15 - 14:15"}		
+			]
+		},
+		{
+			dag: "onsdag",
+			timer: [
+				{fag: "Tysk", tid: "08:30 - 09:30"},
+				{fag: "Matte", tid: "10:15 - 11:15"},
+				{fag: "Samfunnsfag", tid: "12:00 - 13:00"},
+				{fag: "Engelsk", tid: "13:15 - 14:15"},
+				{fag: "Fysak", tid: "15:00 - 16:00"}			
+			]
+		},
+		{
+			dag: "torsdag",
+			timer: [
+				{fag: "Engelsk", tid: "09:00 - 10:00"},
+				{fag: "Gym", tid: "10:15 - 11:15"},
+				{fag: "Naturfag", tid: "12:00 - 13:00"},
+				{fag: "Norsk", tid: "13:15 - 14:15"}			
+			]
+		},
+		{
+			dag: "fredag",
+			timer: [
+				{fag: "Samfunnsfag", tid: "08:30 - 09:00"},
+				{fag: "Mø", tid: "09:00 - 10:00"},
+				{fag: "Matte", tid: "10:15 - 11:15"},
+				{fag: "Gym", tid: "12:00 - 13:00"},
+				{fag: "Kunst og håndtverk", tid: "13:15 - 15:00"}
+			]
+		}
+	]
+
 </script>
 
 <main bind:this={container}>
 
-	<section class="container">
-		{#each timeplanAugust as dag}
+	<section class="container">	
+		{#if august}	
+			{#each timeplanAugust as dag}
+				<div>
+					<article class="day"><h1>{dag.dag}</h1></article>
+					<section class="grid">				
+						{#each dag.timer as time}
+							<article>
+								<h1>{time.fag}</h1>
+								<p>{time.tid}</p>
+							</article>
+						{/each}
+					</section>
+				</div>
+			{/each}
+		{:else}
+		{#each timeplanAksel as dag}
 			<div>
 				<article class="day"><h1>{dag.dag}</h1></article>
 				<section class="grid">				
@@ -77,8 +145,11 @@
 					{/each}
 				</section>
 			</div>
-		{/each}		
+		{/each}
+		{/if}
 	</section>
+	<button on:click={ () => august = false } class={!august ? "aksel selected" : "aksel"}>Aksel</button>
+	<button on:click={ () => august = true } class={august ? "august selected" : "august"}>August</button>
 
 </main>
 
@@ -126,6 +197,25 @@
 		color: white;
 		text-transform: uppercase;
 		height: 3rem;
+	}
+	button {
+		position: absolute;
+		bottom: 1rem;
+		background-color: steelblue;
+		color: white;
+		border-radius: 0.5rem;
+		font-size: 1.5rem;
+		cursor: pointer;
+	}
+	.aksel {
+		left: 1rem;
+	}
+	.august {
+		right: 1rem;
+	}
+	.selected {
+		padding: 1rem;
+		font-weight: bold;
 	}
 </style>
 
